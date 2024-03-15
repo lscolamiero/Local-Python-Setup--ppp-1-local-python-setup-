a07675a6dd254eb160e74841185f3cbb6d2fe988
lscolamiero <lautaro.scolamiero@gmail.com>
2024-03-14 21:32:24 -0700

    second example for eat_lunch function added

--- functions_practice.py
+++ functions_practice.py
@@ -11,7 +11,7 @@ def pack(arg1, arg2, arg3):
 print(pack(1,5,8))
 
 #A function called eat_lunch(). This function should accept a list of any length, and print out a series of strings that say "First I eat __" (the first element of the list), and "Next I eat ___" (for the following elements in the list). If the list is empty, print "My lunchbox is empty!". The function should not return anything.
-def eat_lunch(food_list):
+def eating_lunch(food_list):
     if not food_list:
         print("My lunchbox is empty!")
     else:
@@ -19,4 +19,23 @@ def eat_lunch(food_list):
         for food in food_list[1:]:
             print("Next I eat", food)
 
-eat_lunch(["sandwich", "apple", "chips"])
+eating_lunch([])
+eating_lunch(["apple"])
+eating_lunch(["sandwich", "apple", "chips"])
+
+#Second example for last exercise
+
+def eat_lunch(my_lunch):
+    if len(my_lunch) == 0:
+        print("My lunchbox is empty")
+    for food_index in range(len(my_lunch)):
+        if food_index == 0:
+            print(f"First I eat {my_lunch[food_index]}")
+        elif food_index < len(my_lunch):
+            print(f"Then I eat {my_lunch[food_index]}")
+        else:
+            print("My lunchbox is empty") 
+
+eat_lunch([])
+eat_lunch(["Eggs"])
+eat_lunch(["Eggs", "Beans", "Oatmeal"])
